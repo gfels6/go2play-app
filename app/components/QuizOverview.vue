de12d2ac906704e0e23e6bdb186ff4d87986b7da
gfels6 <sascha.gfeller@gmail.com>
2018-12-31 14:09:09 +0100

    first draft of a quizoverview (more function than design... ;-))

--- app/components/QuizOverview.vue
+++ app/components/QuizOverview.vue
@@ -6,7 +6,27 @@
 
         <Label class="question" :text="tbd" textWrap="true" />
 
-        <Button class='btn' text="Spielen" @tap="changeRoute('quizGame')" />
+        <StackLayout orientation="horizontal" class="lblContainer">
+            <Label class="lbl" :text="lblEnemy" />
+            <Label class="value" :text="this.game.user2" />
+        </StackLayout>
+
+        <StackLayout orientation="horizontal" class="lblContainer">
+            <Label class="lbl" :text="lblScoreUser" />
+            <Label class="value" :text="scoreUser" />
+        </StackLayout>
+
+        <StackLayout orientation="horizontal" class="lblContainer">
+            <Label class="lbl" :text="lblScoreEnemy" />
+            <Label class="value" :text="scoreEnemy" />
+        </StackLayout>
+
+        <StackLayout :class="[{ inactive: !finished }, 'lblContainer']" orientation="horizontal">
+            <Label class="lbl" :text="lblWinning" />
+            <Label class="value" :text="userWon" />
+        </StackLayout>
+
+        <Button :class="[{ inactive: !yourTurn }, 'btn']" text="Spielen" @tap="changeRoute('quizGame')" />
 
     </StackLayout>
   </Page>
@@ -17,6 +37,18 @@
         data() {
             return {
                 tbd: "Hier werden die Spieldetails stehen (Überblick, Wer ist am gewinnen etc)",
+                lblStatus: "",
+                lblEnemy: "Dein Gegner: ",
+                lblScoreUser: "Deine Punkte: ",
+                lblScoreEnemy: "Punkte vom Gegner: ",
+                lblWinning: "Gewonnen hat: ",
+                enemy: "",
+                scoreEnemy: 0,
+                scoreUser: 0,
+                userWon: "",
+                finished: false,
+                yourTurn: false,
+
             };
         },
         name: 'quizoverview-view',
@@ -31,7 +63,21 @@
             },
         },
         mounted() {
-            
+            for (var i = 0; i < this.game.rounds.length; i++) {
+                console.log("PAIR " + i + ": " + this.game.rounds[i].roundNumber);
+                for (let j = 0; j < this.game.rounds[i].gameQuestions.length; j++) {
+                    console.log("question: " + this.game.rounds[i].gameQuestions[j].jntme);
+                }
+            }
+
+            if(this.game.activeUser == "game finished"){
+                this.finished = true;
+                this.userWon = this.game.winner; 
+            }
+
+            if(this.game.activeUser == localStorage.getItem('name')){
+                this.yourTurn = true;
+            }
         }
     }
 </script>
@@ -40,6 +86,24 @@
     ActionBar {
         background-color: #53ba82;
         color: #ffffff;
+    }
+
+    .lbl {
+        margin-left: 25;
+        font-size: 18;
+        color:black;
+        font-weight: bold;
+    }
+
+    .value {
+        margin-left: 5;
+        font-size: 18;
+        color:black;
+        font-weight: bold;
+    }
+
+    .inactive {
+        visibility: collapsed;
     }
 
 </style>
