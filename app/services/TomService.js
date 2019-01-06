104561accbe565a52b96f88ee251fe4f9c251ffb
Gabriel Hess <hessg1@bfh.ch>
2019-01-06 15:39:41 +0100

    Basics for Helping Service (Tom Turnschuh) delete platform folder after pull

---
+++ app/services/TomService.js
@@ -0,0 +1,103 @@
+import { Feedback, FeedbackType, FeedbackPosition } from "nativescript-feedback";
+import { Color } from "tns-core-modules/color";
+
+const feedBack = new Feedback();
+// the color of toms toasts
+const colour = new Color("#444444");
+
+//to be initialized in constructor
+var tom = {img: null};
+var Vibrate = null;
+var vib = null;
+
+
+export default class TomService {
+  /*
+  Constructor of TomService class.
+  The vibrator object must be passed on, because it can't be initialized on
+  android before page is mounted
+
+    parameters  - vibrator: a Vibrator Object
+                - aTom: an Object with 'img' property that is used for displaying Tom in the GUI
+    returns     - nothing
+    author      hessg1
+    version     2019-01-06
+  */
+  constructor(vibrator, aTom){
+    Vibrate  = vibrator; //require("nativescript-vibrate").Vibrate;
+    vib = new Vibrate();
+    tom = aTom;
+  }
+
+  /*
+  Displays the user a message from tom.
+
+    parameters  - title: The title of the notification
+                - text: The main text of the notification
+    returns     -
+    author      hessg1
+    version     2019-01-06
+  */
+  say(title, text) {
+    feedBack.show({
+      title: title,
+      message: text,
+      duration: 10000,
+      backgroundColor: colour,
+      onHide: () => {tom.img = "~/assets/images/tom.png"}
+    });
+    tom.img = "~/assets/images/tom_laugh.png";
+  }
+
+  /*
+  Displays a question from tom and allows the user to pick one of several answers.
+
+    parameters  - title: The title of the notification
+                - text: The main text of the notification
+                - answers: an array with the options the user can pick for answering
+    returns     - a promise with the text of the answer the user picked
+    author      hessg1
+    version     2019-01-06
+  */
+  sayWithOptions(title, text, shorttext, answers) {
+    tom.img = "~/assets/images/tom_laugh.png";
+    return new Promise(function (resolve, reject) {
+      feedBack.show({
+        title: title,
+        message: text,
+        duration: 10000,
+        backgroundColor: colour,
+        onHide: () => {
+          tom.img = "~/assets/images/tom_dontknow.png";
+          action(shorttext, "abbrechen", answers)
+            .then(result => {
+              tom.img = "~/assets/images/tom_laugh.png";
+              if(result == "abbrechen") reject("no response")
+              else resolve(result);
+            });
+        }
+      });
+    });
+  }
+
+
+  /*
+  Sets a Notification Badge on the Tom icon and vibrates the phone
+
+    parameters  - delay: the delay in MS until the notification should be shown
+    returns     - nothing, manipulates GUI
+    author      hessg1
+    version     2019-01-06
+  */
+  notify(delay){
+    setTimeout(()=>{
+      tom.img = "~/assets/images/tom_alert.png";
+      vib.vibrate(200);
+    }, delay);
+
+  }
+
+
+
+
+}
