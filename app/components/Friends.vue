261dbcaddd2e2bb17dbb21888fa1c915d28118bb
Gabriel Hess <hessg1@bfh.ch>
2019-01-09 18:27:17 +0100

    fixed bugfix ;) #last_commit

--- app/components/Friends.vue
+++ app/components/Friends.vue
@@ -51,7 +51,7 @@
 
         // navigates to the main screen
         goHome(){
-          this.$routes(['main'],{ clearHistory: true });
+          this.$navigateTo(this.$routes['main'],{ clearHistory: true });
         },
 
         /*
